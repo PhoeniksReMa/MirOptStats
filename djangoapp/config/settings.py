ae1d04d738f58640f3c202544cd93cfd3c96dedf
MAKSIM REZNICHENKO <maksimrez96@gmail.com>
2026-02-21 13:14:45 +0300

    Поправил докер

--- djangoapp/config/settings.py
+++ djangoapp/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["85.198.82.65", "localhost", "127.0.0.1"]
 
 
 # Application definition
